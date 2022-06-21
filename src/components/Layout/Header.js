f18503d682206c0261770328e7c281674db07c4c
Mudassar755 <mudasar.se@gmail.com>
2022-06-21 23:54:21 +0500

    nav updated

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,4 +1,4 @@
-import * as React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import AlgofanLogo from '../../assets/images/algofan-logo.svg'
@@ -11,30 +11,32 @@ import crypto from '../../assets/images/crypto.png'
 import Telly from '../../assets/images/Telly-Awards.jpeg'
 import Emmy from '../../assets/images/emmy.png'
 import Weby from '../../assets/images/Webby-Awards-Logo.jpg'
-const Header = () => (
-<>
-    <div className="global-styles"></div>
+const Header = () => {
+  const [toggler, setToggler] = useState(false)
+  return (
+    <>
+      <div className="global-styles"></div>
       <div className="html w-embed"></div>
-  <nav className="main-nav">
-  <div className="nav-container">
-    <Link to="/" aria-current="page" className="w-inline-block w--current"><img src={AlgofanLogo} loading="lazy" alt="" className="home-logo" /></Link>
-    <div className="nav-links-wrapper">
-      <div className="nav-links">
-        <Link to="#" className="nav-link">Marketplace</Link>
-        <Link to="/#about-us" className="nav-link">About us</Link>
-        <Link to="/#mission" className="nav-link">Mission</Link>
-        <Link to="/news" className="nav-link">News</Link>
-        <Link to="/contact" className="nav-link">Contact Us</Link>
-      </div>
-    </div>
-    <a data-w-id="7ced627e-3ab6-9674-1eb8-4ba46af274ad" href="#" className="hamburger w-inline-block">
-      <div className="hamburger-line-1" />
-      <div className="hamburger-line-2" />
-    </a>
-  </div>
-</nav>
-</>
-  
-)
+      <nav className="main-nav">
+        <div className="nav-container">
+          <Link to="/" aria-current="page" className="w-inline-block w--current"><img src={AlgofanLogo} loading="lazy" alt="" className="home-logo" /></Link>
+          <div className="nav-links-wrapper" style={{height: toggler ? 'auto' : '0px'}}>
+            <div className="nav-links">
+              <Link to="#" className="nav-link">Marketplace</Link>
+              <Link to="/#about-us" className="nav-link">About us</Link>
+              <Link to="/#mission" className="nav-link">Mission</Link>
+              <Link to="/news" className="nav-link">News</Link>
+              <Link to="/contact" className="nav-link">Contact Us</Link>
+            </div>
+          </div>
+          <a data-w-id="7ced627e-3ab6-9674-1eb8-4ba46af274ad" href="#" className="hamburger w-inline-block" onClick={() => setToggler(!toggler)}>
+            <div className="hamburger-line-1" style={{transform: `translate3d(0px, ${toggler ? '0.5em' : '0em'}, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(${toggler ? '45deg' : '0deg'}) skew(0deg, 0deg)`, transformStyle: 'preserve-3d'}} />
+            <div className="hamburger-line-2" style={{transform: `translate3d(0px, ${toggler ? '-0.8em' : '0em'}, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(${toggler ? '-45deg' : '0deg'}) skew(0deg, 0deg)`, transformStyle: 'preserve-3d'}} />
+          </a>
+        </div>
+      </nav>
+    </>
 
+  )
+}
 export default Header
--- src/components/Layout/SEO.js
+++ src/components/Layout/SEO.js
@@ -38,6 +38,10 @@ const SEO = ({ title, description, canonical }) => {
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:title" content={siteTitle} />
       <meta name="twitter:description" content={siteDesc} />
+      <script src="https://use.typekit.net/vre6bgn.js" type="text/javascript"></script>
+      {/* <script type="text/javascript">try{Typekit.load();}catch(e){}</script> */}
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script>
+      <script dangerouslySetInnerHTML={{ __html: `function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document)`, }} />
     </Helmet>
   )
 }
